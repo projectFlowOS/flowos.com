96a5bef0ee5943a60a89fa1d4c8336c0804a2234
belucat <97773166+sunaxl13@users.noreply.github.com>
2022-07-28 20:45:56 +0800

    gtag.js

---
+++ gtag.js
@@ -0,0 +1,15 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NP6T9PVMGG"></script>
+
+<script>
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('js', new Date());
+
+  gtag('config', 'G-NP6T9PVMGG');
+
+</script>
